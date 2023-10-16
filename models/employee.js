d8e5f8a2e119554b3b1297d24d3f9cb5a9aa3711
valunchanut <valunchanut@outlook.com>
2023-10-15 23:12:36 -0400

    Updated criteria for schema

--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+    first_name: { type: String, required: true, maxlength: 100 },
+    last_name: { type: String, required: true, maxlength: 50 },
+    email: { type: String, required: true, unique: true, maxlength: 50 },
+    gender: { type: String, enum: ['Male', 'Female', 'Other'], maxlength: 25 },
     salary: { type: Number, required: true }
 });
 
--- models/user.js
+++ models/user.js
@@ -2,20 +2,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    username: { type: String, required: true, unique: true, maxlength: 100 },
+    email: { type: String, required: true, unique: true, maxlength: 50 },
+    password: { type: String, required: true, maxlength: 50 }
 });
 
 // Hash password before saving with bcrypt
