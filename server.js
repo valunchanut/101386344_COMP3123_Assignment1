a9a49c2ed9be4995d2393732a221c5ab3b765ac4
valunchanut <valunchanut@outlook.com>
2023-10-15 22:30:38 -0400

    Update server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+
+// Database Connection
+const dbURI = 'mongodb://localhost:27017/comp3123_assigment1';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+  }))
+  .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/emp', employeeRoutes);
+
